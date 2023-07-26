34358af1d7c12e5c8d709bb3ff357a8496a26502
shanakasp <74164029+shanakasp@users.noreply.github.com>
2023-07-26 11:38:23 +0530

    changed 6 files

---
+++ src/Firebse-Config.js
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import * as firebase from "firebase";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDmmdu_VMfa2qi7an1vq_vDhjtvG4s0D04",
+  authDomain: "react-phonenumber-auth-88156.firebaseapp.com",
+  projectId: "react-phonenumber-auth-88156",
+  storageBucket: "react-phonenumber-auth-88156.appspot.com",
+  messagingSenderId: "32904507443",
+  appId: "1:32904507443:web:9ac915ebb3a0327fb7f346",
+  measurementId: "G-MT5M7CC395"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default firebase
