bc7f1da5fd284dff873dd23ded3f841d8a0fa465
shanakasp <74164029+shanakasp@users.noreply.github.com>
2023-07-26 13:28:34 +0530

    Update Firebase-Config.js

--- src/Firebase-Config.js
+++ src/Firebase-Config.js
@@ -3,12 +3,12 @@ import "firebase/auth"; // Importing the authentication service
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDmmdu_VMfa2qi7an1vq_vDhjtvG4s0D04",
-  authDomain: "react-phonenumber-auth-88156.firebaseapp.com",
-  projectId: "react-phonenumber-auth-88156",
-  storageBucket: "react-phonenumber-auth-88156.appspot.com",
-  messagingSenderId: "32904507443",
-  appId: "1:32904507443:web:9ac915ebb3a0327fb7f346",
+  apiKey: "*",
+  authDomain: "*",
+  projectId: "*",
+  storageBucket: "*",
+  messagingSenderId: "*",
+  appId: "*",
 
 };
 
