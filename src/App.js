95c2c0d8b7cb61a168c3f0b1f1a86a47c74d9b27
shanakasp <74164029+shanakasp@users.noreply.github.com>
2023-07-26 13:25:27 +0530

    changed 7 files

--- src/App.js
+++ src/App.js
@@ -1,13 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
-import ContactForm from './ContactForm';
+import React, { Component } from 'react';
+import firebase from './Firebase-Config';
 
-function App() {
-  return (
-    <div className="App">
-      <ContactForm />
-    </div>
-  );
+class App extends Component {
+  componentDidMount() {
+    // Initialize the reCAPTCHA container here
+    window.recaptchaVerifier = new firebase.auth.RecaptchaVerifier('recaptcha', {
+      size: 'invisible',
+      callback: (response) => {
+        // Callback logic if needed
+      },
+      'expired-callback': () => {
+        // Expired callback logic if needed
+      }
+    });
+  }
+
+  handleClick = () => {
+    const number = '+94769125141';
+    firebase
+      .auth()
+      .signInWithPhoneNumber(number, window.recaptchaVerifier)
+      .then((confirmationResult) => {
+        // OTP sent successfully, handle OTP entry
+        const code = prompt('Enter the OTP', '');
+        if (code === null) return;
+
+        confirmationResult
+          .confirm(code)
+          .then((result) => {
+            // Phone number verification successful
+            console.log('User:', result.user);
+            document.querySelector('label').textContent =
+              result.user.phoneNumber + ' Number verified';
+          })
+          .catch((error) => {
+            console.error('OTP Confirmation Error:', error);
+          });
+      })
+      .catch((error) => {
+        console.error('Phone Auth Error:', error);
+      });
+  };
+
+  render() {
+    return (
+      <div>
+        <label></label>
+        <div id="recaptcha"></div>
+        <button onClick={this.handleClick}>Click</button>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/Firebase-Config.js
+++ src/Firebase-Config.js
@@ -1,11 +1,7 @@
-// Import the functions you need from the SDKs you need
-import * as firebase from "firebase";
-
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import firebase from 'firebase/app';
+import "firebase/auth"; // Importing the authentication service
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyDmmdu_VMfa2qi7an1vq_vDhjtvG4s0D04",
   authDomain: "react-phonenumber-auth-88156.firebaseapp.com",
@@ -13,10 +9,10 @@ const firebaseConfig = {
   storageBucket: "react-phonenumber-auth-88156.appspot.com",
   messagingSenderId: "32904507443",
   appId: "1:32904507443:web:9ac915ebb3a0327fb7f346",
-  measurementId: "G-MT5M7CC395"
+
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+firebase.initializeApp(firebaseConfig);
 
-export default firebase
+export default firebase;
